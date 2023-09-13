a9d3c875eca1f9c0f0b233dcd75f0d35816d0ee6
김수현 <watermelon9608@naver.com>
2023-09-13 17:09:06 +0900

    Lv. 1_이상한 문자 만들기

---
+++ Programmers/Lv. 1_이상한 문자 만들기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String s) {
+        String answer;
+        answer = s.toUpperCase();
+        char[] chars = answer.toCharArray();
+
+        //앞문자가 대문자라면 소문자로 치환
+        for (int i = 1; i < chars.length; i++) {
+            if (62 <= chars[i - 1] && chars[i - 1] <= 90) {
+                chars[i] = Character.toLowerCase(chars[i]);
+            }
+        }
+        answer = String.valueOf(chars);
+        return answer;
+    }
+}
